0f666997c9bddfa9ef786cb4953400bd122c93e3
root <root@ns202518.ovh.net>
2012-02-23 18:56:53 +0100

    tmpdir used as --archive-dir parametr to Duplicity (important in >0.6 - does not create a large .cache directory in /root anymore)

--- VZDup.pm
+++ VZDup.pm
@@ -1100,7 +1100,7 @@ else {
     my $cmd = "(";
 #    $cmd .= "duplicity cleanup $dupopts $opts->{destination};";
 #    $cmd .= "duplicity $dupopts --encrypt-key $opts->{key} $snapdir $opts->{destination}";
-    $cmd .= "duplicity --num-retries 20 --allow-source-mismatch $dupopts $snapdir $opts->{destination}/$vmid";
+    $cmd .= "duplicity --num-retries 20 --allow-source-mismatch --archive-dir=$opts->{tmpdir} $dupopts $snapdir $opts->{destination}/$vmid";
 
     $cmd .= ")";
     
