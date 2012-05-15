5b20e4c161a0277acb60bccc1ec081e0cb904568
GDR! <gdr@go2.pl>
2012-05-15 11:28:01 +0200

    Removed dumper

--- VZDup.pm
+++ VZDup.pm
@@ -1023,7 +1023,7 @@ sub exec_backup {
     my $tasklist = [];
 
     if ($opts->{all}) {
-		print Dumper($self->{plugins});
+#		print Dumper($self->{plugins});
 	foreach my $plugin (@{$self->{plugins}}) {
 	    my $vmlist = $plugin->vmlist();
 	    foreach my $vmid (sort @$vmlist) {
